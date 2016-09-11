be27942d2b5d6f282360a12f752b2f9b32b443c7
Zhen Li <lizhen0425@gmail.com>
2016-09-11 11:57:21 -0500

    add sample server and client

---
+++ sampleClient.cpp
@@ -0,0 +1,103 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <string>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <errno.h>
+#include <unistd.h>
+#define MAXLINE 4096
+
+using namespace std;
+
+typedef struct{
+    // protocol version is 3
+    // message type can be 2:JOIN, 3:FWD, 4:SEND
+    uint16_t vrsnType;     
+    uint16_t length;
+}SBCPHeader;
+
+typedef struct{
+    uint16_t type;
+    uint16_t length;
+}SBCPAttrHeader;
+
+int * AttrGen(SBCPAttrHeader* attrHeader, int payloadSize, char* payload){
+    int * attr = malloc(sizeof SBCPAttrHeader + payloadSize);
+    attrHeader->length = 4 + payloadSize;
+    memcpy(attr, payload, payloadSize);
+}
+int * SBCPGen(SBCPHeader* header, vector<int*> payloads){
+    SBCPHeader* ptr = new SBCPHeader;
+    return (int*)ptr;
+}
+
+in_port_t SERV_PORT = 8888;
+string addrStr = "127.0.0.1";
+const char *addr = addrStr.c_str();
+void str_cli(FILE *fp, int sockfd);
+
+int main(int argc ,char *argv[]) {
+    SBCPHeader* lala;
+    SBCPGen(lala);
+    int sockfd;
+    struct sockaddr_in servaddr;
+    
+    memset(&servaddr, 0, sizeof servaddr);
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(SERV_PORT);
+    inet_pton(AF_INET, addr, &servaddr.sin_addr);
+    
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    connect(sockfd, (const struct sockaddr *)&servaddr, sizeof servaddr);
+    
+    str_cli(stdin, sockfd);
+}
+
+void str_cli(FILE *fp, int sockfd) {
+    int maxfd, stdineof, n;
+    fd_set rset;
+    char buf[MAXLINE];
+    FD_ZERO(&rset);
+    stdineof = 0;
+    while(1) {
+        //如果不是已经输入结束,就继续监听终端输入
+        if (stdineof == 0) FD_SET(fileno(fp), &rset);
+        //监听来自服务器的信息
+        FD_SET(sockfd, &rset);
+        //maxfd设置为sockfd和stdin中较大的一个加1
+        maxfd = (fileno(fp) > sockfd ? fileno(fp) : sockfd) + 1;
+        //只关心是否有描述符读就绪,其他几个直接传NULL即可
+        select(maxfd, &rset, NULL, NULL, NULL);
+        
+        //如果有来自服务器的信息可读
+        if (FD_ISSET(sockfd, &rset)) {
+            if ((n = read(sockfd, buf, MAXLINE)) == 0) {
+                //如果这边输入了EOF之后服务器close掉连接说明正常结束，否则为异常结束
+                if (stdineof == 1)
+                    return;
+                else
+                    perror("terminated error\n");
+            }
+            //输出到终端
+            write(fileno(stdout), buf, n);
+        }
+        //如果有来自终端的输入
+        if (FD_ISSET(fileno(fp), &rset)) {
+            //终端这边输入了结束符
+            if ((n = read(fileno(fp), buf, MAXLINE)) == 0) {
+                //标记已经输入完毕，并只单端关闭写，因为可能还有消息在来客户端的路上尚未处理
+                stdineof = 1;
+                shutdown(sockfd, SHUT_WR);
+                //不再监听终端输入
+                FD_CLR(fileno(fp), &rset);
+                continue;
+            }
+            //将输入信息发送给服务器
+            write(sockfd, buf, n);	
+        }
+    }
+}
---
+++ sampleServer.cpp
@@ -0,0 +1,88 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <string>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <errno.h>
+#include <unistd.h>
+#define MAXLINE 4096
+
+in_port_t SERV_PORT = 8888;
+
+int main(int argc ,char *argv[]){
+    int i;
+    int listenfd, connfd, sockfd;
+    int maxfd, maxi, nready, client[FD_SETSIZE];
+    char buf[MAXLINE];
+    struct sockaddr_in cliaddr, servaddr;
+    socklen_t clilen;
+    ssize_t n;
+    fd_set rset, allset;
+    
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    
+    memset(&servaddr, 0, sizeof servaddr);
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(SERV_PORT);
+    
+    if (-1 == bind(listenfd, (struct sockaddr*)&servaddr, sizeof servaddr))
+        printf("bind error\n");
+    
+    listen(listenfd, 1024);
+    
+    //客户端描述符存储在client中,maxi表示该数组最大的存有客户端描述符的数组下标
+    maxfd = listenfd;
+    maxi = -1;
+    memset(client, -1, sizeof client);
+    //初始化读就绪的fd_set数组，并监听listen描述符
+    FD_ZERO(&allset);
+    FD_SET(listenfd, &allset);
+    
+    for (;;) {
+        //allset是监控的描述符列表，rset是可读描述符列表
+        rset = allset;
+        nready = select(maxfd+1, &rset, NULL, NULL, NULL);
+        //如果listen描述符可读，说明有客户端连接
+        if (FD_ISSET(listenfd, &rset)) {
+            clilen = sizeof cliaddr;
+            connfd = accept(listenfd, (struct sockaddr*)&cliaddr, &clilen);
+            if (connfd == -1) perror("accept error\n");
+            else printf("%d accepted!\n", connfd);
+            
+            //扫描client数组，找到下标最小的未用的来存客户端描述符
+            for (i = 0; i < FD_SETSIZE; i++) if (client[i] < 0) {
+                client[i] = connfd;
+                break;
+            }
+            if (i == FD_SETSIZE) perror("too many clients\n");
+            //将客户端描述符放到监视的fd_set中，并更新maxfd和maxi
+            FD_SET(connfd, &allset);
+            if (connfd > maxfd) maxfd = connfd;
+            if (i > maxi) maxi = i;
+            if (--nready <= 0) continue;
+        }
+        //扫描所有的客户端，查看是否有描述符读就绪
+        for (i = 0; i <= maxi; i++) {
+            if ((sockfd = client[i]) < 0) continue;
+            if (FD_ISSET(sockfd, &rset)) {
+                //读到EOF或错误,清除该描述符
+                if ((n = read(sockfd, buf, MAXLINE)) <= 0) {
+                    close(sockfd);
+                    FD_CLR(sockfd, &allset);
+                    client[i] = -1;
+                    if (n < 0) perror("read error\n");
+                    //回显给客户端
+                } else {
+                    write(sockfd, buf, n);
+                }
+                if (--nready <= 0) break;
+            }
+        }
+    }
+    return 0;
+}
